100368bc2fff5b903046d20d65f7d644b9651037
Pavlo Makukha <7734.why.not@gmail.com>
2020-02-09 07:31:31 +0200

    Fetch categories list from db

--- api/src/schemas/index.js
+++ api/src/schemas/index.js
@@ -6,44 +6,7 @@ const {
     GraphQLList
 } = require('graphql');
 
-const categories = [
-    {
-        name: 'Clothes for Men',
-        description: 'A monthly supply of trendy clothes for men.',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        name: 'Clothes for Women',
-        description: 'A monthly supply of trendy clothes for women.',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        name: 'Accessories for Men',
-        description: 'A monthly supply of trendy accessories for men',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        name: 'Accessories for Women',
-        description: 'A monthly supply of trendy accessories for women',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        name: 'Clothes and Accessories for Men',
-        description: 'A monthly supply of trendy clothes and accessories for men',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        name: 'Clothes and Accessories for Women',
-        description: 'A monthly supply of trendy clothes and accessories for women',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-];
+const { Category } = require('../models');
 
 const CategoryType = new GraphQLObjectType({
     name: 'Category',
@@ -61,8 +24,8 @@ const Query = new GraphQLObjectType({
     fields: {
         Categories: {
             type: new GraphQLList(CategoryType),
-            resolve() {
-                return categories;
+            resolve: async () => {
+                return await Category.find();
             }
         }
     }
